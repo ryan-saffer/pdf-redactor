f2c6d8f4c7cb6a5e93b26f457a27e626336992ac
Ryan Saffer <ryansaffer@gmail.com>
2022-06-25 11:37:56 +1000

    First draft working
    Will search the given document for all instances of the given term, and spit out a redacted version of the pdf

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,71 @@ import { PDFNet } from "@pdftron/pdfnet-node"
 
 async function main() {
     const doc = await PDFNet.PDFDoc.createFromFilePath("/Users/rsaffer/Downloads/Application Checklist - Ryan.pdf")
-    console.log(doc)
+    
+    let terms = [
+        "Ryan",
+        "Elazar",
+        "Saffer",
+        "02/05/1993"
+    ]
+
+    let rarr: PDFNet.Redaction[] = []
+
+    for(const pattern of terms) {
+        const redactions = await redact(pattern, doc)
+        rarr = [ ...rarr, ...redactions ]
+    }
+
+    const black = await PDFNet.ColorPt.init(0,0,0,0)
+
+    const app = {
+        redaction_overlay: true,
+        border: false,
+        show_redacted_content_regions: true,
+        positive_overlay_color: black
+    }
+
+    PDFNet.Redactor.redact(doc, rarr, app, false, false);
+    await doc.save('Output/Redacted.pdf', PDFNet.SDFDoc.SaveOptions.e_linearized);
+}
+
+/**
+ * Given a PDFNet.Doc, will return an array of redactions for all occurences of the pattern.
+ * 
+ * @param pattern 
+ * @param doc 
+ * @returns 
+ */
+async function redact(pattern: string, doc: PDFNet.PDFDoc) {
     const txtSearch = await PDFNet.TextSearch.create();
-    let mode = PDFNet.TextSearch.Mode.e_whole_word + PDFNet.TextSearch.Mode.e_page_stop; // Uses both whole word and page stop
-    let pattern = 'Saffer';
-
-    //use regular expression to find credit card number
-    mode += PDFNet.TextSearch.Mode.e_whole_word;
+    let mode = PDFNet.TextSearch.Mode.e_whole_word; // Uses both whole word and page stop
+    mode += PDFNet.TextSearch.Mode.e_whole_word + PDFNet.TextSearch.Mode.e_highlight;
     txtSearch.setMode(mode);
-
-    //call Begin() method to initialize the text search.
     txtSearch.begin(doc, pattern, mode);
-    const result = await txtSearch.run();
+    let output = []
+    while(true) {
+        const result = await txtSearch.run();
 
-    console.log(result)
+        if (result.code === PDFNet.TextSearch.ResultCode.e_found) {
+            console.log(`FOUND TERM: ${pattern}`)
+            // add a link annotation based on the location of the found instance
+            let hlts = result.highlights;
+            await hlts.begin(doc); // is await needed?
+            while (await hlts.hasNext()) {
+                const quadArr = await hlts.getCurrentQuads();
+                for (let i = 0; i < quadArr.length; ++i) {
+                    const currQuad = quadArr[i];
+                    const x1 = Math.min(Math.min(Math.min(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
+                    const x2 = Math.max(Math.max(Math.max(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
+                    const y1 = Math.min(Math.min(Math.min(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
+                    const y2 = Math.max(Math.max(Math.max(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
 
-    if (result.code === PDFNet.TextSearch.ResultCode.e_found) {
-        // add a link annotation based on the location of the found instance
-        let hlts = result.highlights;
-        await hlts.begin(doc); // is await needed?
-        while (await hlts.hasNext()) {
-            const curPage = await doc.getPage(await hlts.getCurrentPageNumber());
-            const quadArr = await hlts.getCurrentQuads();
-            console.log(quadArr)
-            for (let i = 0; i < quadArr.length; ++i) {
-                const currQuad = quadArr[i];
-                const x1 = Math.min(Math.min(Math.min(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
-                const x2 = Math.max(Math.max(Math.max(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
-                const y1 = Math.min(Math.min(Math.min(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
-                const y2 = Math.max(Math.max(Math.max(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
-
-                const hyperLink = await PDFNet.LinkAnnot.create(doc, await PDFNet.Rect.init(x1, y1, x2, y2));
-                await hyperLink.setAction(await PDFNet.Action.createURI(doc, 'http://www.pdftron.com'));
-                await curPage.annotPushBack(hyperLink);
+                    output.push(await PDFNet.Redactor.redactionCreate(await hlts.getCurrentPageNumber(), await PDFNet.Rect.init(x1, y1, x2, y2), false, ''))
+                }
+                hlts.next();
             }
-            hlts.next();
+        } else {
+            return output
         }
     }
 }
